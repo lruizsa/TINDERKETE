6e6f2dac1388f8fa969438e4fb725a1f6b8fc154
oaginagabo23wg <oaginagabo23wg@ikzubirimanteo.com>
2024-11-26 11:18:29 +0100

    Erreserbak

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';
 import Register from './erregistratu';
+import ErreserbakForm from './erreserbakForm';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/erreserbak" element={<ErreserbakForm />} />
       </Routes>
     </Router>
   );
