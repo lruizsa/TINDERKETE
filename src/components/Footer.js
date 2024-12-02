d507b808fad3c3d8be87d49a7ebdfa040a25c2b1
imarticorenair <imarticorenair23wg@ikzubirimanteo.com>
2024-12-02 10:05:44 +0100

    footerra aldatzen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,59 @@
 import React from 'react';
-import './footer.css'; 
+import './footer.css';
 import img1 from '../images/facebookAplicacion.png';
 import img2 from '../images/logoInsta.png';
 import img3 from '../images/Xaplicacion.png';
 
 function Footer() {
   return (
-    <footer className="text-white text-center py-2">
-      <div className="container">
-        <p className="mb-1" style={{ fontSize: '0.8rem' }}>
-          &copy; {new Date().getFullYear()} Tinderkete. All rights reserved.
-        </p>
-        <p className="mb-2" style={{ fontSize: '0.8rem' }}>
-          <a href="/privacy" className="text-white text-decoration-none">
-            Privacy Policy
-          </a>{' '}
-          |{' '}
-          <a href="/terms" className="text-white text-decoration-none">
-            Terms of Service
-          </a>
-        </p>
+    <footer>
+      {/* Nivel superior */}
+      <div className="footer-top text-white text-center py-3">
+        <div className="container">
+          <p className="mb-1" style={{ fontSize: '0.8rem' }}>
+            &copy; {new Date().getFullYear()} Tinderkete. All rights reserved.
+          </p>
+          <p className="mb-2" style={{ fontSize: '0.8rem' }}>
+            <a href="/privacy" className="text-white text-decoration-none">
+              Privacy Policy
+            </a>{' '}
+            |{' '}
+            <a href="/terms" className="text-white text-decoration-none">
+              Terms of Service
+            </a>
+          </p>
+        </div>
+      </div>
 
-        <div className="d-flex justify-content-center gap-2 mt-1">
-          <a href="#" onClick={(e) => e.preventDefault()}>
-            <img
-              src={img1}
-              alt="Facebook"
-              className="img-fluid"
-              style={{ maxHeight: '20px' }}
-            />
-          </a>
-          <a href="#" onClick={(e) => e.preventDefault()}>
-            <img
-              src={img2}
-              alt="Instagram"
-              className="img-fluid"
-              style={{ maxHeight: '20px' }}
-            />
-          </a>
-          <a href="#" onClick={(e) => e.preventDefault()}>
-            <img
-              src={img3}
-              alt="X"
-              className="img-fluid"
-              style={{ maxHeight: '20px' }}
-            />
-          </a>
+      {/* Nivel inferior */}
+      <div className="footer-bottom text-white text-center py-2">
+        <div className="container">
+          <div className="d-flex justify-content-center gap-2 mt-1">
+            <a href="#" onClick={(e) => e.preventDefault()}>
+              <img
+                src={img1}
+                alt="Facebook"
+                className="img-fluid"
+                style={{ maxHeight: '20px' }}
+              />
+            </a>
+            <a href="#" onClick={(e) => e.preventDefault()}>
+              <img
+                src={img2}
+                alt="Instagram"
+                className="img-fluid"
+                style={{ maxHeight: '20px' }}
+              />
+            </a>
+            <a href="#" onClick={(e) => e.preventDefault()}>
+              <img
+                src={img3}
+                alt="X"
+                className="img-fluid"
+                style={{ maxHeight: '20px' }}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
