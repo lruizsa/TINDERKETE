19d2943672494ffe6b38d15491769d55ee901fc8
lruizsa <lruizsa23wg@ikzubirimanteo.com>
2024-12-04 13:41:40 +0100

    App.js- gauzak komentatuta ondo funtzionatzeko

--- src/components/App.js
+++ src/components/App.js
@@ -7,30 +7,31 @@ import Txapelketak from './Txapelketak';
 import Produktuak from './Produktuak';
 import Hasiera from './Hasiera';
 import PartidoakCard from './PartidoakCard'; 
+import PerfilaIkusi from './PerfilaIkusi'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Kontaktu from './Kontaktuacomp';
 import MapaLista from './MapaLista';
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import englishContent from "./lang/en.json";
-import spanishContent from "./lang/es.json";
+//import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import englishContent from "./lang/en.json";
+// import spanishContent from "./lang/es.json";
 
-i18n.use(initReactI18next).init({
-    resources: {
-        en: englishContent,
-        es: spanishContent,
-    },
+// i18n.use(initReactI18next).init({
+//     resources: {
+//         en: englishContent,
+//         es: spanishContent,
+//     },
 
-    fallbackLng: "en",
+//     fallbackLng: "en",
 
-    interpolation: {
-        escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    },
-});
+//     interpolation: {
+//         escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+//     },
+// });
 
 
 
