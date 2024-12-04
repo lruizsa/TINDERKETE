738f72bc6a02a479001942c8feea264289cc19ee
oaginagabo <oaginagabo23wg@ikzubirimanteo.com>
2024-12-04 12:51:18 +0100

    Hizkuntzak WIP (estoMarchaBienTxabale)

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Kontaktu from './Kontaktuacomp';
 import MapaLista from './MapaLista';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import englishContent from "./lang/en.json";
+import spanishContent from "./lang/es.json";
 
+i18n.use(initReactI18next).init({
+    resources: {
+        en: englishContent,
+        es: spanishContent,
+    },
 
+    fallbackLng: "en",
 
+    interpolation: {
+        escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+});
 
 
 
