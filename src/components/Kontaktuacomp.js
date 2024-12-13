ff26da8837aa12f3f6c8817c72b121f4a1f59fc1
lruizsa <lruizsa23wg@ikzubirimanteo.com>
2024-12-13 07:29:03 +0000

    Hover-a aldatu

--- src/components/Kontaktuacomp.js
+++ src/components/Kontaktuacomp.js
@@ -85,7 +85,7 @@ function Kontaktu() {
           <div className="mb-4">
             <button
               type="submit"
-              className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               {t('kontaktua.bidali')}
             </button>
